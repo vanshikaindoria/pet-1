3a0b1f92ae791e5e3a5ab876c6928fe70464195a
vanshikaindoria <80957954+vanshikaindoria@users.noreply.github.com>
2021-07-02 16:56:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var  dog, happyDog, database, foodS, foodStock;
+
+function preload()
+{
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
